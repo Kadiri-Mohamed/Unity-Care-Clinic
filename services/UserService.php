b004fe73118b1db8b907aed4d2f98717d16fa741
Kadiri <kadiri.moohamed@gmail.com>
2026-01-05 16:25:35 +0100

    feat: refactor UserService with validation, update method and improved naming

--- services/UserService.php
+++ services/UserService.php
@@ -2,29 +2,62 @@
 
 class UserService
 {
-    private UserRepository $userRepository;
+    private UserRepository $userRepo;
 
     public function __construct()
     {
-        $this->userRepository = new UserRepository();
+        $this->userRepo = new UserRepository();
     }
 
-    public function createUser(array $data): bool
+    public function validate(array $data): void
     {
-        if (!Validator::required($data['nom'])) {
+        if (!Validator::required($data['nom'] ?? null)) {
             throw new Exception("Nom is required");
         }
 
-        if (!Validator::email($data['email'])) {
+        if (!Validator::required($data['prenom'] ?? null)) {
+            throw new Exception("Prenom is required");
+        }
+
+        if (!Validator::email($data['email'] ?? null)) {
             throw new Exception("Invalid email");
         }
 
-        if (!Validator::minLength($data['password'], 6)) {
+        if (!Validator::required($data['username'] ?? null)) {
+            throw new Exception("Username is required");
+        }
+
+        if (!Validator::required($data['role'] ?? null)) {
+            throw new Exception("Role is required");
+        }
+    }
+
+    public function create(array $data): int
+    {
+        if (!Validator::minLength($data['password'] ?? '', 6)) {
             throw new Exception("Password too short");
         }
 
+        $this->validate($data);
+
         $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
 
-        return $this->userRepository->create($data);
+        return $this->userRepo->create($data);
+    }
+
+    public function update(int $id, array $data): bool
+    {
+        $this->validate($data);
+
+        if (!empty($data['password'])) {
+            if (!Validator::minLength($data['password'], 6)) {
+                throw new Exception("Password too short");
+            }
+            $data['password'] = password_hash($data['password'], PASSWORD_DEFAULT);
+        } else {
+            unset($data['password']);
+        }
+
+        return $this->userRepo->update($id, $data);
     }
 }
