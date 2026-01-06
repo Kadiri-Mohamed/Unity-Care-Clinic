6426728005fbf81941fc40702a033777f4711d91
Kadiri <kadiri.moohamed@gmail.com>
2026-01-06 15:05:48 +0100

    feat: add auto redirect to login page

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ require_once 'autoload.php';
 
 $db = new Database();
 $conn = $db->dbConnection();
+echo "<script>location.href = './auth/login.php';</script>";
 
 // if(!$conn) {
 //     echo "Database connection failed!\n";
